902d2fc36a0db7c780f3b8d5d2f59517defadb46
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2024-01-30 16:13:58 +0000

    Create multi_data.py

---
+++ Reverse_Cipher/multi_data.py
@@ -0,0 +1,27 @@
+"""
+
+The reverse cipher.
+
+Program accepts a txt file of credit card details, or some other secure data.
+
+The file of data is read, reversed and printed to a new file
+
+"""
+
+
+# Open the file in write mode
+encrypted_cc_details = open("cc_details_encrypted.txt", "w")
+ 
+
+with open("cc_details_uncrypted.txt", "r") as uncrypted_cc_details:
+
+    uncrypted_cc_data = uncrypted_cc_details.read()
+ 
+
+encrypted_cc_data = uncrypted_cc_data[::-1]
+ 
+
+encrypted_cc_details.write(encrypted_cc_data)
+ 
+encrypted_cc_details.close()
+
